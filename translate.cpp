9e53f7a33a45145f569abe17e6b8e8cfc57c6328
Phil Huffman <56phil@users.noreply.github.com>
2020-06-05 19:48:56 -0500

    Adds logic that handles file not found.

--- translate.cpp
+++ translate.cpp
@@ -1,9 +1,10 @@
 // FILE:        translate.cpp
 // STANDARD:    C++17
 // AUTHOR:      Phil Huffman
+// Copyright 2020 Philip Huffman
 //
 // PURPOSE:
-// This is a python program that reads a text file and writes a text file with
+// This is a c++ program that reads a text file and writes a text file with
 // a name derived from the name of the input file (e.g. d.txt -> d.OUT.txt).
 // It reads the first data set, determines where the protein sequence is in the
 // count. Then, in every subsequent data set, the program will only return data
@@ -20,7 +21,8 @@
 // The general tasks are:
 // 1. Parse command line building a vector of file names
 // 2. Instantiate a DataSet object
-// 3. Repeat step 2 until all file names have been used.
+// 3. Instantiate a DataSet object
+// 4. Repeat from step 2 until all file names have been used.
 //
 // CLASSES:
 //  InputParser
@@ -36,29 +38,28 @@
 #include <array>
 #include <fstream>
 #include <iostream>
-#include <sstream>
 #include <string>
 #include <vector>
 
 class
 InputParser {
     public:
-        InputParser (int &argc, char **argv){
+        InputParser(int &argc, char **argv) {
             for (int i=1; i < argc; ++i)
                 this->tokens.push_back(std::string(argv[i]));
         }
 
-        const std::string& getCmdOption(const std::string &option) const{
+        const std::string& getCmdOption(const std::string &option) const {
             std::vector<std::string>::const_iterator itr;
             itr =  std::find(this->tokens.begin(), this->tokens.end(), option);
-            if (itr != this->tokens.end() && ++itr != this->tokens.end()){
+            if (itr != this->tokens.end() && ++itr != this->tokens.end()) {
                 return *itr;
             }
             static const std::string empty_string("");
             return empty_string;
         }
 
-        bool cmdOptionExists(const std::string &option) const{
+        bool cmdOptionExists(const std::string &option) const {
             return std::find(this->tokens.begin(), this->tokens.end(), option)
                 != this->tokens.end();
         }
@@ -70,46 +71,50 @@ InputParser {
 class
 DataSet {
     public:
-
         DataSet(const std::string fname, const std::string out_id,
                 char sub_char, int line_length) {
-
             this->ifn = fname;
             this->ofn = fname;
             this->oid = out_id + ".";
             this->sc = sub_char;
             this->ll = line_length;
 
-            make_ofn();
             read_file();
-            parse_raw();
-            get_results();
+            make_ofn();
+            if (this->raw.size() > 0) {
+                parse_raw();
+                get_results();
+            } else {
+                std::cerr << "No data extracted from " << ifn << std::endl;
+            }
         }
 
         void write_to_file() {
-            long l;     // length of output line
-            long b;     // start of output line in sequence
-            DataSet_item dsi;   // current dataset item
-            std::ofstream out;
-            out.open(this->ofn);
-            if(out.is_open()) {
-                for(auto i = data.begin(); i != data.end(); i++) {
-                    dsi = *i;
-                    out << dsi.name << std::endl;
-                    for(b = 0; b < dsi.seq.size(); b += this->ll) {
-                        l = this->ll;
-                        while(b + l > dsi.seq.size()) {
-                            l--;
+            if (this->data.size() > 0) {
+                int l;              // length of output line
+                int b;              // start of output line in sequence
+                DataSet_item dsi;   // current dataset item
+                std::ofstream out;
+                out.open(this->ofn);
+                if (out.is_open()) {
+                    for (auto i = data.begin(); i != data.end(); i++) {
+                        dsi = *i;
+                        out << dsi.name << std::endl;
+                        for (b = 0; b < dsi.seq.size(); b += this->ll) {
+                            l = this->ll;
+                            while (b + l > dsi.seq.size()) {
+                                l--;
+                            }
+                            out << dsi.seq.substr(b, l) << std::endl;
                         }
-                        out << dsi.seq.substr(b, l) << std::endl;
                     }
+                } else {
+                    std::cerr << ofn << " was not opened." << std::endl;
                 }
-                std::cout << this->data.size() << " data set items written to "
-                    << this->ofn << std::endl;
-            } else {
-                std::cerr << ofn << " was not opened." << std::endl;
+                out.close();
             }
-            out.close();
+            std::cout << this->data.size() << " data set items written to "
+                << this->ofn << std::endl;
         }
 
     private:
@@ -128,10 +133,12 @@ DataSet {
         std::vector<std::string> raw;       // raw data from input file
 
         void read_file() {
-            std::ifstream file(this->ifn);
-            std::string str;
-            while (std::getline(file, str)) {
-                this->raw.emplace_back(str);
+            if (!this->ifn.empty()) {
+                std::ifstream file(this->ifn);
+                std::string str;
+                while (std::getline(file, str)) {
+                    this->raw.emplace_back(str);
+                }
             }
         }
 
@@ -139,10 +146,10 @@ DataSet {
             DataSet_item dsi;
             dsi = this->data[0];
             std::string seq_0 = dsi.seq;
-            for(long n = 1; n < data.size(); n++) {
+            for (int n = 1; n < data.size(); n++) {
                 dsi = this->data[n];
-                for(long i = 0; i < dsi.seq.size(); i++) {
-                    if(!isalpha(seq_0[i])) {
+                for (int i = 0; i < dsi.seq.size(); i++) {
+                    if (!isalpha(seq_0[i])) {
                         dsi.seq[i] = this->sc;
                     }
                 }
@@ -150,7 +157,7 @@ DataSet {
             }
         }
 
-        std::string get_name(std::string s) {
+        std::string get_name(std::string &s) {
             int start = s.find('[');
             int end = s.find(']');
             int len = end - start;
@@ -158,9 +165,9 @@ DataSet {
         }
 
         void cleanup_seq(std::string &s) {
-            long i = 0;
-            while(i < s.size()) {
-                if(s[i] == '\n') {
+            int i = 0;
+            while (i < s.size()) {
+                if (s[i] == '\n') {
                     s.erase(i, 1);
                 } else {
                     i++;
@@ -179,9 +186,9 @@ DataSet {
             dsi.name = "";
             dsi.seq = "";
             std::string line = "";
-            for(auto e = this->raw.begin(); e != this->raw.end(); e++) {
+            for (auto e = this->raw.begin(); e != this->raw.end(); e++) {
                 line = *e;
-                if(line[0] == '>') {
+                if (line[0] == '>') {
                     augment_data(dsi);
                     dsi.name = get_name(line);
                     dsi.seq = "";
@@ -194,17 +201,18 @@ DataSet {
 
         void augment_data(DataSet_item &dsi) {
             cleanup_seq(dsi.seq);
-            if(!dsi.name.empty() && !dsi.seq.empty()) {
+            if (!dsi.name.empty() && !dsi.seq.empty()) {
                 this->data.emplace_back(dsi);
             }
         }
 };
 
 int
-main(int argc, char **argv){
+main(int argc, char **argv) {
     InputParser input(argc, argv);
-    if(input.cmdOptionExists("-h")){
-        std::cout << "Usage: translate -s + -l 60 -o OUT <at least one file name>\n"
+    if (input.cmdOptionExists("-h")) {
+        std::cout << "Usage: translate -s + -l 60 -o OUT "
+            << "<at least one file name>\n"
             << "\ts:\tSubstitution char, default = '+'\n"
             << "\t\tOnly first character will be used.\n"
             << "\tl:\tMax output length, default = 60\n"
@@ -215,38 +223,31 @@ main(int argc, char **argv){
 
     int lineLen = 60;
     const std::string &lineLength = input.getCmdOption("-l");
-    if(lineLength.size() > 0) {
+    if (lineLength.size() > 0) {
         lineLen = std::stoi(lineLength);
     }
 
     std::string out_id = "OUT";
     const std::string &oout_id = input.getCmdOption("-o");
-    if(oout_id.size() > 0) {
+    if (oout_id.size() > 0) {
         out_id = oout_id;
     }
 
     char sc = '+';
     const std::string &substitutionChar = input.getCmdOption("-s");
-    if(substitutionChar.size() > 0) {
+    if (substitutionChar.size() > 0) {
         sc = substitutionChar[0];
     }
 
-    std::vector <std::string> fns;
     int i = 1;
-    while(i < argc) {
-        if(argv[i][0] == '-') {
+    while (i < argc) {
+        if (argv[i][0] == '-') {
             i++;
         } else {
-            fns.emplace_back(argv[i]);
+            DataSet data(argv[i], out_id, sc, lineLen);
+            data.write_to_file();
         }
         i++;
-    }
-
-    std::string ts;
-    for (auto i = fns.begin(); i !=fns.end(); i++) {
-        ts = *i;
-        DataSet data(ts, out_id, sc, lineLen);
-        data.write_to_file();
     }
 
     return 0;
